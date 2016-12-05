ffc60652a4c5255cfd9ee92f69024fe9c2f4aaf0
Jang, Kyoungrok <kyoungrok.jang@gmail.com>
2016-12-05 16:45:35 +0900

    working with dataproc scripts

--- dataproc-scripts/create-clusters.sh
+++ dataproc-scripts/create-clusters.sh
@@ -3,4 +3,4 @@ gcloud dataproc clusters create gs1-sample \
     --project irnlp-gs1 \
     --bucket irnlp-gs1 \
     --initialization-actions \
-        gs://irnlp-gs1/scripts/dataproc-init/jupyter.sh
+        gs://irnlp-gs1/scripts/dataproc-init/mecab.sh
---
+++ dataproc-scripts/init/mecab.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+
+MECAB_KO_FNAME=mecab-0.996-ko-0.9.2
+MECAB_KO_URL=https://bitbucket.org/eunjeon/mecab-ko/downloads/$MECAB_KO_FNAME.tar.gz
+MECAB_KO_DIC_FNAME=mecab-ko-dic-2.0.1-20150920
+MECAB_KO_DIC_URL=https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/$MECAB_KO_DIC_FNAME.tar.gz
+
+mkdir -p /tmp
+TEMP_DIR=/tmp
+
+mecab-ko() {
+    echo "Installing mecab-ko"
+    wget -O $TEMP_DIR/mecab-ko.tar.gz $MECAB_KO_URL
+    tar xzf $TEMP_DIR/mecab-ko.tar.gz -C $TEMP_DIR 
+    cd $TEMP_DIR/$MECAB_KO_FNAME
+    ./configure && make && make check && make install
+}
+
+mecab-ko-dic() {
+    echo "Installing mecab-ko-dic"
+    wget -O $TEMP_DIR/mecab-ko-dic.tar.gz $MECAB_KO_DIC_URL
+    tar xzf $TEMP_DIR/mecab-ko-dic.tar.gz -C $TEMP_DIR
+    cd $TEMP_DIR/$MECAB_KO_DIC_FNAME
+    ./configure && make && make check && make install
+}
+
+mecab-python() {
+    # Install Miniconda / conda
+    ./dataproc-initialization-actions/conda/bootstrap-conda.sh
+    # Update conda root environment with specific packages in pip and conda
+    CONDA_PACKAGES=''
+    PIP_PACKAGES='mecab-python3'
+    CONDA_PACKAGES=$CONDA_PACKAGES PIP_PACKAGES=$PIP_PACKAGES ./dataproc-initialization-actions/conda/install-conda-env.sh
+}
+
+# run
+mecab-ko
+mecab-ko-dic
+mecab-python
