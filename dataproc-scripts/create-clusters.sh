ee261920e870ae1548130056098e185a8be14c8d
Jang, Kyoungrok <kyoungrok.jang@gmail.com>
2016-12-05 16:04:55 +0900

    add dataproc-init scripts

---
+++ dataproc-scripts/create-clusters.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+gcloud dataproc clusters create gs1-sample \
+    --project irnlp-gs1 \
+    --bucket irnlp-gs1 \
+    --initialization-actions \
+        gs://irnlp-gs1/scripts/dataproc-init/jupyter.sh
