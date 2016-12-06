88b3622c872817c7eba9a6f779d3ef71630dbbd0
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2016-12-07 00:22:01 +0900

    disable tag filtering

--- dataproc/submit-job.sh
+++ dataproc/submit-job.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud beta dataproc jobs submit pyspark --async --cluster gs1-fhee hello-world.py
+gcloud dataproc jobs submit pyspark --async --cluster gs1-fhee hello-world.py
--- jobs/3_food_extract_tags.py
+++ jobs/3_food_extract_tags.py
@@ -11,7 +11,7 @@ from pyspark.sql import Row
 from konlpy.tag import Mecab
 
 APP_NAME = "Food - Mecab"
-PREFIX = ''
+PREFIX = 'gs://irnlp-gs1/'
 DATA_FILE = '{}data/food.csv.gz'.format(PREFIX)
 DATA_PARQUET = '{}output/food.parquet'.format(PREFIX)
 TAGS_PARQUET = '{}output/food_pos.parquet'.format(PREFIX)
@@ -20,13 +20,11 @@ OUTPUT_DIR = '{}output'.format(PREFIX)
 def extract_tags(spark):
     """Extract tags from df"""
     df = spark.read.parquet(TAGS_PARQUET)
-    target_tags = ['NNG', 'NNP', 'NNB', 'NNBC', 'NR', 'NP',
-                   'JK.*', 'JX', 'JC']
-    tag_pattern = re.compile('(' + '|'.join(target_tags) + ')')
-    tag_sentences = df.rdd.map(lambda r: ' '.join(['{}_{}'.format(token, tag) for (token, tag) in r.tags if tag_pattern.match(tag)]).strip())
+    # target_tags = ['NNG', 'NNP', 'NNB', 'NNBC', 'NR', 'NP',
+                #    'JK.*', 'JX', 'JC']
+    # tag_pattern = re.compile('(' + '|'.join(target_tags) + ')')
+    tag_sentences = df.rdd.map(lambda r: ' '.join(['{}_{}'.format(token, tag) for (token, tag) in r.tags]).strip())
     tag_sentences.saveAsTextFile(os.path.join(OUTPUT_DIR, 'food_pos_sentences.text'))
-    # df_tag_sentences = spark.createDataFrame(tag_sentences)
-    # df_tag_sentences.write.save(os.path.join(OUTPUT_DIR, 'food_pos_sentences.parquet'), format='parquet', mode='overwrite')
 
 def main(spark):
     """Main function
