00303921f75e8a04e43ac6063ee20078cf472c21
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-12-06 20:26:50 +0900

    fix get_fhee_inverted_dict

--- jobs/utils.py
+++ jobs/utils.py
@@ -1,20 +1,24 @@
 import csv
+import re
 
 def get_fhee_inverted_dict(dict_path='../dict/fhee_dict.tsv'):
     """Get FHEE inverted dictionary (category => word)
-    
+
+    Blanks and special characters (e.g. -) in a single multi-word is removed.
+
     Args:
         dict_path (str): path to dictionary file
-    
+
     Returns:
         inverted_dict (:obj:dict)
     """
     inverted_dict = {}
+    non_char_pattern = re.compile(r'[^가-힣a-zA-Z]+', re.UNICODE)
 
     with open(dict_path, 'r', encoding='utf-8') as fin:
         reader = csv.DictReader(fin, delimiter='\t')
         for item in reader:
-            category, word = item['category'].strip(), item['word'].strip()
+            category, word = item['category'].strip(), re.sub(non_char_pattern, '', item['word'])
             inverted_dict[word] = category
 
     return inverted_dict
