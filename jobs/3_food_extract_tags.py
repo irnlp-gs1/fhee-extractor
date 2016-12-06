12d365b1aa67c46d28d329341350c780cb2dee46
Jang, Kyoungrok <kyoungrok.jang@gmail.com>
2016-12-06 13:21:42 +0900

    tag filtering  추가

--- jobs/3_food_extract_tags.py
+++ jobs/3_food_extract_tags.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from __future__ import unicode_literals
 import os
+import re
 from operator import add
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
@@ -10,7 +11,7 @@ from pyspark.sql import Row
 from konlpy.tag import Mecab
 
 APP_NAME = "Food - Mecab"
-PREFIX = 'gs://irnlp-gs1/'
+PREFIX = ''
 DATA_FILE = '{}data/food.csv.gz'.format(PREFIX)
 DATA_PARQUET = '{}output/food.parquet'.format(PREFIX)
 TAGS_PARQUET = '{}output/food_pos.parquet'.format(PREFIX)
@@ -19,9 +20,13 @@ OUTPUT_DIR = '{}output'.format(PREFIX)
 def extract_tags(spark):
     """Extract tags from df"""
     df = spark.read.parquet(TAGS_PARQUET)
-    tag_sentences = df.rdd.map(lambda r: Row(uid=r.uid, tag_sentence=' '.join(['{}_{}'.format(token, tag) for (token, tag) in r.tags]).strip()))
-    df_tag_sentences = spark.createDataFrame(tag_sentences)
-    df_tag_sentences.write.save(os.path.join(OUTPUT_DIR, 'food_pos_sentences.parquet'), format='parquet', mode='overwrite')
+    target_tags = ['NNG', 'NNP', 'NNB', 'NNBC', 'NR', 'NP',
+                   'JK.*', 'JX', 'JC']
+    tag_pattern = re.compile('(' + '|'.join(target_tags) + ')')
+    tag_sentences = df.rdd.map(lambda r: ' '.join(['{}_{}'.format(token, tag) for (token, tag) in r.tags if tag_pattern.match(tag)]).strip())
+    tag_sentences.saveAsTextFile(os.path.join(OUTPUT_DIR, 'food_pos_sentences.text'))
+    # df_tag_sentences = spark.createDataFrame(tag_sentences)
+    # df_tag_sentences.write.save(os.path.join(OUTPUT_DIR, 'food_pos_sentences.parquet'), format='parquet', mode='overwrite')
 
 def main(spark):
     """Main function
